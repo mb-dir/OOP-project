30979e9da4af81a7a8ee51a866a5bd5d92bb4a6c
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-01-04 22:20:54 +0100

    Rid of unnecessary code and config tests environment

--- src/departementGUI.java
+++ src/departementGUI.java
@@ -1,10 +1,6 @@
 import javax.swing.*;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
